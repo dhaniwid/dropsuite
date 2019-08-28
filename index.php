6600833695d29b8498853b734b4cf8aae4256675
Ramadhani Widodo <real.ramadhani.widodo@gmail.com>
2019-08-28 12:19:19 +0700

    Commit the first part of file and folder reading

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php 
+/* BEGIN */
+
+class content {
+	static public $array_file = array();
+}
+
+/* A function to iterate file(s) inside a directory */
+function iterate_dir($dirinfo){
+	$dir = new DirectoryIterator($dirinfo->getPathName());
+
+	while ($dir->valid()){
+		// iterate over all file(s)
+		foreach ($dir as $fileinfo) {
+			if (!$fileinfo->isDot()) {
+				if ($fileinfo->isDir()) {
+					# iterate through the dir
+					iterate_dir($fileinfo);
+				} elseif ($fileinfo->isFile()) {
+					// If it's a file then let's read it
+			    	read_file_contents($fileinfo);
+				}
+			}
+		}
+	}
+}
+
+/* A function to read the file content then push in to array */
+function read_file_contents($fileinfo) {
+	$content = file_get_contents($fileinfo->getPathName());	
+	# push into the static array
+	content::$array_file[] = $content;	
+}
+
+###############################################################
+# START
+# Initial
+$directory = "DropsuiteTest/";        // Directory	
+
+# initiate the first iterator
+$dir = new DirectoryIterator($directory);
+
+foreach ($dir as $fileinfo) {
+	// Omit . and .. entries
+	if (!$fileinfo->isDot()){
+		// Let's see if it's a directory/folders
+		if ($fileinfo->isDir()) {
+			# iterate through the dir
+			iterate_dir($fileinfo);
+		} elseif ($fileinfo->isFile()) {
+			// If it's a file then let's read it
+	    	read_file_contents($fileinfo);
+		}
+	}	    
+}
+echo '</ol>';
+
+echo '<pre>';
+$arr = array_count_values(content::$array_file);
+$maxs = array_keys($arr, max($arr));
+print_r($maxs[0]);
+print_r(max($arr));
+echo '</pre>';
+# FINISH
+##############################################################
+
+
+/* END */
+?>
